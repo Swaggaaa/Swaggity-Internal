c2574aac91dd73d2a2cdb54d6cddb3c792aa1d29
FishCoding <neo.darkon@gmail.com>
2016-09-10 12:09:17 +0200

    Lmao

---
+++ Swaggity Internal/stdafx.h
@@ -0,0 +1,30 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+#include <dwmapi.h>
+#pragma comment(lib, "dwmapi.lib")
+#include <string>
+#include <TlHelp32.h>
+#include <random>
+#include <sstream>
+#include <fstream>
+#include <io.h>
+#include <iostream>
+#include <algorithm>
+#include <future>
+
+#include "D3D.h"
+#include "IO.h"
+// TODO: reference additional headers your program requires here
